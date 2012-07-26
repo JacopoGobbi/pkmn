69a9a8f0913666bad27adfe62b9070d0f2a1071d
pspgt <gabry.t92@gmail.com>
2012-07-26 17:33:59 +0200

    modified:   bin/DevelopmentPanel.class 	modified:   bin/Map.class 	modified:   bin/Maps.class 	modified:   bin/MenuListener.class 	modified:   bin/Percorso102.class 	modified:   bin/Percorso104.class 	modified:   bin/Petalipoli.class 	modified:   bin/PokemonImages.class 	modified:   bin/PokemonPanel.class 	modified:   bin/Solarosa.class 	modified:   bin/Window.class 	modified:   src/Percorso104.java 	modified:   src/Petalipoli.java 	modified:   src/PokemonImages.java 	modified:   src/PokemonPanel.java 	modified:   src/Solarosa.java 	modified:   src/Window.java

--- src/PokemonImages.java
+++ src/PokemonImages.java
@@ -7,16 +7,16 @@ public class PokemonImages {
 		pokemon = new ImageIcon[386][2];
 		for(i=0; i < 386; ++i) {
 			if(i < 10) {
-				pokemon[i][0] = new ImageIcon("./pkmn/00" + (i+1) + "(1)(hgss).png");
-				pokemon[i][1] = new ImageIcon("./pkmn/00" + (i+1) + "(2)(hgss).png");
+				pokemon[i][0] = new ImageIcon("../pkmn/00" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("../pkmn/00" + (i+1) + "(2)(hgss).png");
 			}
 			else if(i < 100) {
-				pokemon[i][0] = new ImageIcon("./pkmn/0" + (i+1) + "(1)(hgss).png");
-				pokemon[i][1] = new ImageIcon("./pkmn/0" + (i+1) + "(2)(hgss).png");
+				pokemon[i][0] = new ImageIcon("../pkmn/0" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("../pkmn/0" + (i+1) + "(2)(hgss).png");
 			}
 			else {
-				pokemon[i][0] = new ImageIcon("./pkmn/" + (i+1) + "(1)(hgss).png");
-				pokemon[i][1] = new ImageIcon("./pkmn/" + (i+1) + "(2)(hgss).png");
+				pokemon[i][0] = new ImageIcon("../pkmn/" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("../pkmn/" + (i+1) + "(2)(hgss).png");
 			}				
 		}
 	}
@@ -24,16 +24,16 @@ public class PokemonImages {
 		pokemon = new ImageIcon[386][2];
 		for(i=0; i < 386; ++i) {
 			if(i < 10) {
-				pokemon[i][0] = new ImageIcon("./" + folder + "/00" + (i+1) + "(1)(hgss).png");
-				pokemon[i][1] = new ImageIcon("./" + folder + "/00" + (i+1) + "(2)(hgss).png");
+				pokemon[i][0] = new ImageIcon("../" + folder + "/00" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("../" + folder + "/00" + (i+1) + "(2)(hgss).png");
 			}
 			else if(i < 100) {
-				pokemon[i][0] = new ImageIcon("./" + folder + "/0" + (i+1) + "(1)(hgss).png");
-				pokemon[i][1] = new ImageIcon("./" + folder + "/0" + (i+1) + "(2)(hgss).png");
+				pokemon[i][0] = new ImageIcon("../" + folder + "/0" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("../" + folder + "/0" + (i+1) + "(2)(hgss).png");
 			}
 			else {
-				pokemon[i][0] = new ImageIcon("./" + folder + "/" + (i+1) + "(1)(hgss).png");	
-				pokemon[i][1] = new ImageIcon("./" + folder + "/" + (i+1) + "(2)(hgss).png");	
+				pokemon[i][0] = new ImageIcon("../" + folder + "/" + (i+1) + "(1)(hgss).png");	
+				pokemon[i][1] = new ImageIcon("../" + folder + "/" + (i+1) + "(2)(hgss).png");	
 			}
 		}
 	}
---
+++ src/PokemonImages.java~
@@ -0,0 +1,40 @@
+import javax.swing.ImageIcon;
+
+public class PokemonImages {
+	ImageIcon pokemon[][];
+	int i;
+	PokemonImages() {
+		pokemon = new ImageIcon[386][2];
+		for(i=0; i < 386; ++i) {
+			if(i < 10) {
+				pokemon[i][0] = new ImageIcon("./pkmn/00" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("./pkmn/00" + (i+1) + "(2)(hgss).png");
+			}
+			else if(i < 100) {
+				pokemon[i][0] = new ImageIcon("./pkmn/0" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("./pkmn/0" + (i+1) + "(2)(hgss).png");
+			}
+			else {
+				pokemon[i][0] = new ImageIcon("./pkmn/" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("./pkmn/" + (i+1) + "(2)(hgss).png");
+			}				
+		}
+	}
+	PokemonImages(String folder) {
+		pokemon = new ImageIcon[386][2];
+		for(i=0; i < 386; ++i) {
+			if(i < 10) {
+				pokemon[i][0] = new ImageIcon("./" + folder + "/00" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("./" + folder + "/00" + (i+1) + "(2)(hgss).png");
+			}
+			else if(i < 100) {
+				pokemon[i][0] = new ImageIcon("./" + folder + "/0" + (i+1) + "(1)(hgss).png");
+				pokemon[i][1] = new ImageIcon("./" + folder + "/0" + (i+1) + "(2)(hgss).png");
+			}
+			else {
+				pokemon[i][0] = new ImageIcon("./" + folder + "/" + (i+1) + "(1)(hgss).png");	
+				pokemon[i][1] = new ImageIcon("./" + folder + "/" + (i+1) + "(2)(hgss).png");	
+			}
+		}
+	}
+}
